e3d4e9418e1347e8c9f2d11d95d01cf73b43017e
Sarovar Shaikh <sarwarshaikh3622@gmail.com>
2022-08-06 18:23:33 +0530

    add

---
+++ index.js
@@ -0,0 +1,37 @@
+// qus 1---
+//what is Javascript?
+//Javascript is a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else.
+
+
+
+// qus 2--
+//what is the javascript run time environment?
+//A runtime environment is where your program will be executed. 
+//The JavaScript engine works inside an environment, which provides additional features to your scripts that you can use at runtime.
+//JRE is responsible for making JavaScript asynchronous.
+
+
+
+// qus 3---
+//what is execution context?
+//Execution context is basically abstract concept of an environment where the Javascript code is evaluated and executed. Whenever any code is run in JavaScript, it’s run inside an execution context.
+//Execution context has two types :1.Global Execution Context and 2.Functional Execution Context.
+//This is the default or base execution context. The code that is not inside any function is in the global execution context.
+//Every time a function is invoked, a brand new execution context is created for that function. Each function has its own execution context called functional execution conetext.
+
+
+
+
+//qus 4---
+//what is javascript Engine?
+//JavaScript is not understandable by computer but the only browser understands JavaScript. 
+//So, we need a program to convert our JavaScript program into computer-understandable language. 
+//Hencw,A JavaScript engine is a computer program that executes JavaScript code and converts it into computer understandable language.
+
+
+
+
+//qus5----
+///flow of data in engine?
+//The environment needs to have an engine, which takes the JS code that's written in human-readable syntax and turns it into machine code.
+//The engine uses a parser to go through the code line by line and check if the syntax is correct. If there are any errors, code will stop executing and an error will be thrown.
